9d78df6f5c30c19da2c8dca90b823427ff8c99b6
delkabo <delkabo@yandex.ru>
2025-05-14 23:37:22 +0300

    video_division

--- gitprojs/pythontrain/video_division/main.py
+++ gitprojs/pythontrain/video_division/main.py
@@ -1,151 +1,175 @@
 import os
-import datetime
+import time
+import subprocess
+from datetime import datetime
+from datetime import timedelta
 from moviepy import *
 #from moviepy.editor import *
 from pydub import AudioSegment
+import wave
+import soundfile as sf
+import contextlib
 import sys
 
 sys.path.append('/usr/bin')
 
+re=[]
+musics_dir='musics'
 
 def edit_text():
-
+#####################
+# part editor
 #    # work with music
 
-#    if not os.path.exists('data'):
-#        os.mkdir('data')
+# Get name and info
+    if not os.path.exists('data'):
+        os.mkdir('data')
 
-#    get_path = r'./'
-#    name_sh = r'.mp3'
-#    get_name = [_ for _ in os.listdir(get_path) if _.endswith(name_sh)]
-#    get_name = get_name[0]
-#    print(get_name)
+    get_path = r'./'
+    name_sh = r'.mp3'
+    get_name = [_ for _ in os.listdir(get_path) if _.endswith(name_sh)]
+    print("get_name", get_name[0])
+    get_name = get_name[0]
+    # print(get_name)
 
-#    # new_name = os.path.join('file.mp3')
-#    new_name = 'file.mp3'
-#    os.rename(get_name, new_name)
+    new_name = os.path.join('file.mp3')
+    new_name = 'file.mp3'
+    os.rename(get_name, new_name)
 
-#    new_title = get_name.strip('.mp3').replace('-', ' ')
-#    print(new_title)
+    new_title = get_name.strip('.mp3').replace('-', ' ')
+    print("Новое имя file.mp3 new_title. Результат: ", new_title)
 
-#    music_long = AudioSegment.from_file('file.mp3', format='mp3')
-#    # short_music = music_long[:10000]
-#    # short_music.export('data/charp1.mp3', format='mp3')
+    info_mp3 = sf.info(new_name)
+    duration = int(info_mp3.frames / info_mp3.samplerate)
+    print(f"Длительность: {duration} сек")
+    duration=time.strftime('%H:%M:%S', time.gmtime(duration))
+    print("Длительность в формате %H:%M:%S ", duration)
 
-#    long_len = len(music_long)/1000
-#    print(long_len)
 
-#    len_hours = float(long_len/60)//60
-#    print(" long hours " + str(len_hours))
 
-#    len_mus_minute = str(long_len % 60).split('.')[:1]
-#    print(len_mus_minute)
+    # with contextlib.closing(wave.open(new_name,'r')) as f:
+    #     frames = f.getnframes()
+    #     rate = f.getframerate()
+    #     music_long = frames / float(rate)
+    #     print(music_long)
 
-#    len_mus_sc = str(51.64).split('.')[-1:]
-#    len_mus_sc = len_mus_sc[0]
-#    print(len_mus_sc)
+    # music_long = AudioSegment.from_file('file.mp3', format='mp3')
+    # short_music = music_long[:10000]
+    # short_music.export('data/charp1.mp3', format='mp3')
 
-#    # transform long music from second to minute
+    # long_len = len(music_long)/1000
+    # print(long_len)
 
-#    # text_info = input('Insert info with timeline: ')
+    # len_hours = float(long_len/60)//60
+    # print(" long hours " + str(len_hours))
+
+    # len_mus_minute = str(long_len % 60).split('.')[:1]
+    # print(len_mus_minute)
+
+    # len_mus_sc = str(51.64).split('.')[-1:]
+    # len_mus_sc = len_mus_sc[0]
+    # print(len_mus_sc)
+
+#    transform long music from second to minute
+
+#    text_info = input('Insert info with timeline: ')
 
 #    # with open('timeline.txt', 'a') as file:
 #    #     file.write(text_info)
-
+###########################3
 #    # work with text
+    # re = []
 
-    re = []
+
+
+# Calculate time for even music
     re_next=[]
 
     file_t = open('timeline.txt', 'r')
     sum_lines = len(file_t.readlines())
-    print(sum_lines)
-    file_t.close()	
+    print('Количество строк в файле: ', sum_lines)
+    file_t.close()
     file_t = open('timeline.txt', 'r')
-    time_code_prew=""
+    time_code_prew=0
+    time_code_end=0
+    time_code=0
     # получить длину видео
-    for i in range(sum_lines)
+    count_x=0
+    for i in range(sum_lines):
         line = file_t.readline()
-        if not line:
-            break
-        time_code = '-'.join(line.split(' ')[:+1]).strip()
+        # if not line:
+        #     break
+# Загоняем в переменную время и название, получаем следующее время, вычитаем 1 и кладём в кортеж
+        if i > 0:
+            # print("счётчик -----: ", i)
+            if (i > sum_lines):
+                print("БОЛЬШЕ sum_lines--------------------------")
+                time_code_end=duration
+                print("ДЛИНА time_code_end",len(time_code_end))
+            else:
+                time_code = '-'.join(line.split(' ')[:+1]).strip()
+                time_code_end = time_code
+            # time.sleep(10)
+            if len(time_code_end) <= 5:
+                # print("TEST time_code", str(time_code))
+                time_code_end = datetime.strptime(time_code, '%M:%S')
+                time_code_end = time_code_end - timedelta(seconds=1)
+                time_code_end = time_code_end.strftime('%M:%S')
+            else:
+                time_code_end = datetime.strptime(time_code_end, '%H:%M:%S')
+                time_code_end = time_code_end - timedelta(seconds=1)
+                time_code_end = time_code_end.strftime('%H:%M:%S')
+            print(str(time_code_prew),':',str(time_code_end) + ' - ' + time_text)
+            re.append({
+                'time_code_start': time_code_prew,
+                'time_code_end': time_code_end,
+                'time_text': time_text_prew
+            })
+            time_code_prew = time_code
+        
         time_text = ' '.join(line.split(' ')[1:]).strip()
-        if time_code_prew == "":
-        	time_code_prew = time_code
-        	time_text_prew = time_text
-        else
-        	# print(time_code + ' - ' + time_text)
-        	time_v = datetime.strptime(time_code, '%H:%M:%S')
-        	time_v = time_v - timedelta(seconds=1)
-        	time_code = time_v.strftime('%H:%M:%S')
-		re.append({
-		    'time_code_start': time_code_prew,
-		    'time_code_end': time_code,
-		    'time_text': time_text_prew
-		})
-        # Добавить массив и обращаться к предыдущему, если пустой то ничего не добавлять. обращаться к предыдущему. Или переменные.
-       # 
-#    re.append({
-#        'time_code': '',
-#        'time_code_end': '',
-#        'time_text': ''
-#    })
-    
-    print(re)
+        time_text_prew = time_text
+
+
+    with open('timeline.txt', 'r') as file_t:
+        lines = file_t.read().splitlines()
+        data_info_playlist = lines[-1]
+    time_code_prew = '-'.join(data_info_playlist.split(' ')[:+1]).strip()
+    time_code_end = "6:59:59"
+    time_text_prew = ' '.join(line.split(' ')[1:]).strip()
+    re.append({
+            'time_code_start': time_code_prew,
+            'time_code_end': duration,
+            'time_text': time_text_prew
+        })
 
     file_t.close()
+    print(re)
 
-#    time_save = ''
-#    info_save = ''
-#    numb = 0
-#    for item in re:
-#        numb = numb + 1
-#        try:
-#            ts = ':'.join(str(item.get('time_code')).split(':')[-1:]).strip()
-#            tm = ':'.join(str(item.get('time_code')).split(':')[:1]).strip()
-#            bm = int(tm)
-#            b = int(ts)
-#        except:
-#            pass
+    if not os.path.exists(musics_dir):
+        os.makedirs(musics_dir)
 
-#        try:
-#            if bm == 0 and b == 0:
-#                tm = '0' + str(bm)
-#                ts = '0' + str(b)
-#            elif b == 0 and bm != 0:
-#                b = 60
-#                b = b - 1
-#                bm = bm - 1
-#                if bm < 10:
-#                    tm = '0' + str(bm)
-#                ts = str(b)
-#            elif b < 10:
-#                b = b - 1
-#                ts = '0' + str(b)
-#            else:
-#                b = b - 1
-#                ts = str(b)
-#        except:
-#            tm = ''
-#            ts = ''
+    music_long = AudioSegment.from_file('file.mp3', format='mp3')
+    a = AudioSegment.from_mp3(new_name) 
+    # short_music = music_long[:10000]
+    # short_music.export('data/charp1.mp3', format='mp3')
 
-#        if numb != 1:
-#            end_time = tm + ':' + ts
-#            print(str(time_save) + ' - ' + str(end_time) + ' ' + str(info_save))
+    # first_second = a[:1000] # get the first second of an mp3 
+    slice = a[5000:10000] # get a slice from 5 to 10 seconds of an mp3
+    slice.export('musics/charp1.mp3', format='mp3')
+    duration = int(info_mp3.frames / info_mp3.samplerate)
+    print(f"Длительность: {duration} сек")
 
-#        time_save = item.get('time_code')
-#        info_save = item.get('time_text')
+def print_array():
+    # print(re)
+    for x1 in re:
+        print(x1["time_text"])
 
-#        # re_vid = re[list_vid].get('time_code')
-#        # print(str(re_vid) + ' - ' + str(tm) + ':' + ts)
-
-#        # print(ts)
-#        # print(str(time_save) + ' ' + str(item.get('time_code')))
-#        # time_save = item.get('time_code')
 
 
 def main():
     edit_text()
+    print_array()
 
 
 if __name__ == '__main__':
